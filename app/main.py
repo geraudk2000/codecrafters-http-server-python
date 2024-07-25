e2e818655862c215d293b56a283cf2760e80a452
Geraud KAMENI <geraudkameni@gmail.com>
2024-07-25 12:14:03 +0200

    uncomment change

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ def main():
     print("Logs from your program will appear here!")
 
     #
-    # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket.accept() # wait for client
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket.accept() # wait for client
 
 
 if __name__ == "__main__":
