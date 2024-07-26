dae72c2bf911adc40092a141ad3dc42e1f002c2d
Geraud KAMENI <geraudkameni@gmail.com>
2024-07-26 11:35:52 +0200

    add response

--- app/main.py
+++ app/main.py
@@ -7,7 +7,20 @@ def main():
 
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n") # wait for client
+    #server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n") # wait for client
+
+    conn, addr = server_socket.accept()
+    with conn:
+        while True: 
+            data = conn.recv(1024)
+            request_data = data.decode().split("\r\n")
+            response = b"HTTP/1.1 200 OK\r\n\r\n"
+            if not data:
+                break
+            #conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            if request_data[0].split(" ")[1] != "/":
+                response =  b"HTTP/1.1 404 Not Found\r\n\r\n"
+            conn.sendall(response)
 
 
 if __name__ == "__main__":
