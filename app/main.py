379c60a92fcaeb050927e966e995f150d0d36378
Geraud KAMENI <geraudkameni@gmail.com>
2024-07-29 04:22:20 +0200

    Read header

--- app/main.py
+++ app/main.py
@@ -21,20 +21,24 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, client_addr = server_socket.accept()
     
-    with client_socket:
-        
+    with client_socket: 
         data = client_socket.recv(1024)
+        print(data)
         method, path, version = parse_request(data.decode())
+        #print(method, path, version) 
         print(path)
         if path == "/": 
             client_socket.sendall(OK_RESPONSE)
         elif path.startswith("/echo/"):
             string = path.lstrip("/echo/")
             response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
-        
-            print(response)
             client_socket.sendall(response)
-                
+        elif path.startswith("/user-agent"):
+            string = data.decode().split(":")[-1].lstrip(" ").rstrip("\r\n\r\n")
+            print(string)
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
+            print(response)
+            client_socket.sendall(response)    
         else:
             client_socket.sendall(NOTFOUND_RESPONSE)
             
