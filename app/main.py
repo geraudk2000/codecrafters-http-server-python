7a7913fdd1f9e61a066b7cbd745b4dcfcc40e938
Geraud KAMENI <geraudkameni@gmail.com>
2024-08-01 08:28:04 +0200

    Gzip compression

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ def handle_request(client_socket, client_address):
                 encode_string = encoding.group(0)
                 compress_body = gzip.compress(encode_string.encode())
             string = path.lstrip("/echo/")
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: {encode_string}\r\nContent-Length: {len(compress_body)}\r\n\r\n".encode() + compress_body
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: {string}\r\nContent-Length: {len(compress_body)}\r\n\r\n".encode() + compress_body
             #print(response)
             #client_socket.sendall(response)
         elif path.startswith("/user-agent"):
